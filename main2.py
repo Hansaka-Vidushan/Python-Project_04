98917f1428331a618abbe8d6b46d63849d27fa5a
Hansaka-Vidushan <hansakavidushan56@gmail.com>
2025-05-02 12:55:45 +0530

    Explain if condition for two methods

---
+++ main2.py
@@ -0,0 +1,15 @@
+# Method 01
+height = 160
+army = ""
+
+if height > 150:
+    print("Selected")
+else:
+    print("Rejected")
+
+# Method 02
+
+height = 155
+
+army = "Selected" if height > 150 else "Rejected"
+print(army)
